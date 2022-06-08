1e11f0b140245c9457d4e8881c62ad14d8b17571
wilmargalvis <ing.wilmargalvis@hotmail.com>
2022-06-07 22:35:37 -0500

    frases al reves y Encontrar múltiplos

---
+++ FrasesAlReves/FrasesAlReves/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrasesAlReves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "Bienvenido a Treda Solutions";
+
+            string[] vec = frase.Split(' ');
+
+            for (int i = 0; i < vec.GetLength(0); i++)
+            {
+                if (vec[i].Length>5) {
+                    string FraseAux = vec[i];
+                    string FraseDevuelta="";
+                    for (int j = FraseAux.Length-1; j >=0; j--)
+                    {
+                        FraseDevuelta += FraseAux[j];
+                    }
+
+                    vec[i] = FraseDevuelta;
+                }
+
+            }
+
+            //Imprimir Frase devuelta
+            for (int i = 0; i < vec.GetLength(0); i++)
+            {
+                Console.Write(vec[i]+ " ");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
